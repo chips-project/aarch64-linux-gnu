33c35f0bd9df2a51e63912a6e903b340f23f2b19
Ahmad Thoriq Najahi <najahi@chips-projects.xyz>
2020-06-06 11:17:36 +0700

    aarch64-linux-gnu-gcc (GCC) 11.0.0 20200605 (experimental)
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

---
+++ lib/gcc/aarch64-linux-gnu/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/najahi/workspace/.build/aarch64-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=aarch64-linux-gnu --prefix=/home/najahi/x-tools/aarch64-linux-gnu --with-sysroot=/home/najahi/x-tools/aarch64-linux-gnu/aarch64-linux-gnu/sysroot --enable-languages=c,c++ --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --disable-libmpx --with-gmp=/home/najahi/workspace/.build/aarch64-linux-gnu/buildtools --with-mpfr=/home/najahi/workspace/.build/aarch64-linux-gnu/buildtools --with-mpc=/home/najahi/workspace/.build/aarch64-linux-gnu/buildtools --with-isl=/home/najahi/workspace/.build/aarch64-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/najahi/x-tools/aarch64-linux-gnu/aarch64-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
